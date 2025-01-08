50bb7cb8da1d7710fa8362b3a77e17968ce21fc9
laz0rshi <laz0rshi@protonmail.com>
2025-01-07 20:45:32 -0500

    updated vm-install

--- scripts_tools/vm-install-set-up.sh
+++ scripts_tools/vm-install-set-up.sh
@@ -3,19 +3,23 @@
 #####################################################################
 # Variables Declaration
 # List of packages to install
-packages=(
-code
-golang
+packagelist_1=(
+kali-linux-everything
+apt-transport-https
 sqlmap
 mfsconsule
+pure-ftpd
+mssql-cli
+)
+
+packagelist_2=(
+code
+golang
 blododhoudnd
 rustscan
 terminator
 ligolo
 atftp
-kali-linux-everything
-pure-ftpd
-mssql-cli
 redis-tools
 cmake
 putty
@@ -25,12 +29,25 @@ putty
 # Global Variables
 PROJECT_DIR="$HOME/$project"
 #####################################################################
-# Make directories 
-# Network layout
-mkdir -p "$PROJECT_DIR/network_layout"
-# mkdir $PROJECT_DIR
-for i in "${ip_list[@]}" 
+
+###########
+# Prepare to install VScode
+wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
+echo "deb [arch=amd64,arm64,armhf signed-by=/etc/apt/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" |sudo tee /etc/apt/sources.list.d/vscode.list > /dev/null
+rm -f packages.microsoft.gpg
+###########
+
+for i in "${packagelist_1[@]}" 
 do 
     sudo apt install -y "$i"
 done
+
+
+for i in "${packagelist_2[@]}" 
+do 
+    sudo apt install -y "$i"
+done
+
+
 

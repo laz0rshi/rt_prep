224f09214fc3fde1206782b41a44eec20da8b792
laz0rshi <167032603+laz0rshi@users.noreply.github.com>
2025-01-28 20:49:47 -0500

    Update set-up.sh

--- scripts_tools/set-up.sh
+++ scripts_tools/set-up.sh
@@ -1,67 +1,157 @@
 #!/bin/bash
+
 # project_build.sh
-#####################################################################
-# Variables Declaration
+
+########################################################
+
+# Set Global variables
+
 # Name of the project
+
 PROJECT=""
+
 PROJECT_DIR=""
+
+FILE_LAYOUT=(
+
+evidence/credentials
+
+evidence/data
+
+evidence/screenshot
+
+logs
+
+network
+
+scans/nmap
+
+scope
+
+tools
+
+)
+
+# Set project specfic variables
+
 # List of IPs for the initial scan and setup
+
 ip_list=(
 
-
 )
-FILE_LAYOUT=(
-evidence/credentials
-evidence/data
-evidence/screenshot
-logs
-network
-scans/nmap
-scope
-tools
-)
-#####################################################################
-# Make directories 
-## File Layout
-#└───Project
-#    ├───evidence
-#    │   ├───credentials
-#    │   ├───data
-#    │   └───screenshots
-#    ├───logs
-#    ├───network
-#    ├───scans 
-#    ├───scope
-#    └───tools
 
-read -p "Enter your desired project name: " PROJECT
-PROJECT_DIR="$HOME/offsec/tj_list/$PROJECT"
+############################################################
 
-read -p "Enter your desired project directory: (default:$PROJECT_DIR)" DIR
-PROJECT_DIR="$HOME/offsec/tj_list/$PROJECT/$DIR"
-read -p "$PROJECT_DIR is this correc? (Y/N)" YorN
-	case $YorN in
-	    [Yy]*) echo "Great"  ;;
-	    [Nn]*) echo "Aborted" ; exit;;
-	esac
+############################################################
 
-read -p "Enter the target IP: " IP
+# Help                                                     #
 
-echo -e "Making $PROJECT_DIR directory"
-mkdir -p $PROJECT_DIR
+############################################################
 
+Help()
 
-for i in "${FILE_LAYOUT[@]}" 
-do 
-    echo "Making $PROJECT_DIR$i directories"
-    mkdir -p "$PROJECT_DIR/$i"
+{
+
+   # Display Help
+
+   echo "Creates the folder structure for a target"
+
+   echo
+
+   echo "Syntax: scriptTemplate [-i|h|n|v]"
+
+   echo "options:"
+
+   echo "i                 IP address of host."
+
+   echo "h     Print this Help."
+
+   echo "p                Project name."
+
+   echo "v     Verbose mode."
+
+   echo
+
+}
+
+############################################################
+
+############################################################
+
+# Main program                                             #
+
+############################################################
+
+############################################################
+
+ 
+
+ 
+
+############################################################
+
+# Process the input options. Add options as needed.        #
+
+############################################################
+
+# Get the options
+
+while getopts "i:h:p:" option; do
+
+   case $option in
+
+                  i) # Enter IP address
+
+                                IP=$OPTARG;;
+
+      h) # display Help
+
+         Help
+
+         exit;;
+
+      p) # Enter a name
+
+         PROJECT=$OPTARG
+
+                                PROJECT_DIR="$HOME/offsec/tj_list/$PROJECT" ;;
+
+     \?) # Invalid option
+
+         echo "Error: Invalid option"
+
+         exit;;
+
+   esac
+
 done
 
-echo "$IP" >> "$PROJECT_DIR/network/ips.txt"
+ 
 
-for i in "${ip_list[@]}" 
-do 
-    echo 'Making the "$i" directories'
-   #mkdir -p "$PROJECT_DIR/scans/$i" 
-    echo "$i" >> "$PROJECT_DIR/network/ips.txt"
+shift "$(( OPTIND - 1 ))"
+
+ 
+
+if [ -z "$IP" ] || [ -z "$PROJECT" ]; then
+
+        echo 'Missing -i or -p' >&2
+
+        exit 1
+
+fi
+
+ 
+
+echo -e "Making $PROJECT_DIR directory"
+
+mkdir -p $PROJECT_DIR
+
+for i in "${FILE_LAYOUT[@]}"
+
+do
+
+    echo "Making $PROJECT_DIR$i directories"
+
+    mkdir -p "$PROJECT_DIR/$i"
+
 done
